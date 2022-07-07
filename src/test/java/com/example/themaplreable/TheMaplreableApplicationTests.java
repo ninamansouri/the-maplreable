c913ab9e0e0e3e90156ab6b2539b6011bc4b5e7f
gregory <gregory.boeroteyssier@maplr.co>
2022-07-07 12:42:57 -0500

    first commit

---
+++ src/test/java/com/example/themaplreable/TheMaplreableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.themaplreable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMaplreableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
